ccdf12d817307b2b877ffe18750bfc2e2627890f
Sarwan Gupta <sarwan.sep@gmail.com>
2019-10-27 13:41:08 +0000

    while with Docker

---
+++ JavaHelloWorld.js
@@ -0,0 +1,8 @@
+public class JavaHelloWorld
+{
+        public static void main (String [] args)
+        {
+        System.out.printin("Java Hello World ");
+        System.out.printin("Hello again ");
+        }
+}
